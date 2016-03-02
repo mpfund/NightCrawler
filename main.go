5fa9f185351e8037a8fe1ead28a048eda112ddd0
Marinus <BlackEspresso>
2016-03-02 07:17:34 +0100

    move to gin

--- main.go
+++ main.go
@@ -1,51 +1,47 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
-	"net/url"
 	"time"
 
-	"github.com/miekg/dns"
+	"github.com/gin-gonic/gin"
 
 	"./httpmitm"
 	"./servertasks"
 	"github.com/BlackEspresso/crawlbase"
 	"github.com/BlackEspresso/htmlcheck"
-	"github.com/PuerkitoBio/goquery"
-	"github.com/robertkrimen/otto"
+
 	"gopkg.in/mgo.v2"
 )
 
-type Response struct {
-	Page       *crawlbase.Page
-	HtmlErrors []*htmlcheck.ValidationError
-}
-
 var session *mgo.Session
+var db *mgo.Database
+
+var userAgentHeader string = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
 
 func main() {
-	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.URL.Path[1:])
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-	http.HandleFunc("/", staticSites)
-	http.HandleFunc("/test", testSite)
-	http.HandleFunc("/api/crawl", apiCrawlRequest)
-	http.HandleFunc("/api/dcrawl", apiDynamicCrawlRequest)
-	http.HandleFunc("/api/addTag", apiAddTag)
-	http.HandleFunc("/api/runscript", apiRunScript)
-	http.HandleFunc("/api/proxyrequests", apiProxyRequests)
-	http.HandleFunc("/tasks", servertasks.GenHandler(gHandler))
+
+	r := gin.Default()
+
+	r.Static("/static", "./static")
+
+	//r.GET("/login", login)
+	r.GET("/test", testSite)
+
+	r.GET("/api/getsiteinfo", apiGetSiteInfo)
+	r.GET("/api/addTag", apiAddTag)
+	r.GET("/api/proxyrequests", apiProxyRequests)
+	//r.GET("/tasks", servertasks.GenHandler(gHandler))
+
 	var err error
 	session, err = mgo.Dial("localhost")
+	db = session.DB("nightcrawler")
+
 	if err != nil {
 		panic(err)
 	}
+
 	defer session.Close()
 
 	servertasks.Start()
@@ -54,59 +50,8 @@ func main() {
 		simpleProxyHandler := http.HandlerFunc(httpmitm.GenSimpleHandlerFunc(req, resp))
 		http.ListenAndServe(":8081", simpleProxyHandler)
 	}()
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
 
-func gHandler(t *servertasks.TaskBlock, r *http.Request) {
-	js := r.FormValue("jsFunc")
-	t.Func = func(task *servertasks.TaskBlock) {
-		vm := newJSRuntime()
-		k, err := vm.Run(js)
-		task.Done = true
-		if err == nil {
-			txt, err2 := k.ToString()
-			if err2 != nil {
-				task.ErrorText = err2.Error()
-			} else {
-				task.SuccessText = txt
-			}
-		} else {
-			task.ErrorText = err.Error()
-		}
-	}
-}
-
-func resolveDNS(call otto.FunctionCall) otto.Value {
-	name, err := call.Argument(0).ToString()
-	if err != nil {
-		return otto.UndefinedValue()
-	}
-
-	config, _ := dns.ClientConfigFromFile("./resolv.conf")
-	c := new(dns.Client)
-
-	m := new(dns.Msg)
-	m.SetQuestion(dns.Fqdn(name), dns.TypeANY)
-	m.RecursionDesired = true
-
-	r, _, err := c.Exchange(m, net.JoinHostPort(config.Servers[0], config.Port))
-	if err != nil {
-		k, _ := otto.ToValue("")
-		return k
-	}
-	resp := ""
-	for _, v := range r.Answer {
-		resp += v.String()
-		resp += "\n"
-	}
-	respV, _ := otto.ToValue(resp)
-	return respV
-}
-
-func newJSRuntime() *otto.Otto {
-	vm := otto.New()
-	vm.Set("resolveDNS", resolveDNS)
-	return vm
+	r.Run(":8080")
 }
 
 func req(r *http.Request) {
@@ -114,115 +59,89 @@ func req(r *http.Request) {
 }
 
 func resp(r *http.Request, req *http.Response, dur time.Duration) {
-	page := PageFromResponse(r, req, dur)
-	c := session.DB("checkSite").C("requests")
+	cw := crawlbase.NewCrawler()
+	page := cw.PageFromResponse(r, req, dur)
+	c := db.C("requests")
 	c.Insert(page)
 }
 
-func staticSites(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadFile("index.html")
-	logFatal(err)
-	w.Write(b)
+func testSite(g *gin.Context) {
+	inpage, _ := g.GetQuery("inpage")
+	inscript, _ := g.GetQuery("inscript")
+	ineval, _ := g.GetQuery("ineval")
+
+	g.String(200, "<html>"+inpage+"<script>var m = '"+inscript+
+		"';document.write(m);document.cookie='test='+m</script>"+
+		"<script>eval("+ineval+")</script>"+
+		"<script>document.write(decodeURIComponent(document.location.hash))</script>"+
+		"</html>")
 }
 
-func testSite(w http.ResponseWriter, r *http.Request) {
-	inpage := r.URL.Query().Get("inpage")
-	inscript := r.URL.Query().Get("inscript")
-	ineval := r.URL.Query().Get("ineval")
-
-	w.Write([]byte("<html>" + inpage + "<script>var m = '" + inscript +
-		"';document.write(m);document.cookie='test='+m</script>" +
-		"<script>eval(" + ineval + ")</script>" +
-		"<script>document.write(decodeURIComponent(document.location.hash))</script>" +
-		"</html>"))
-}
-
-func apiRunScript(w http.ResponseWriter, r *http.Request) {
-	script := r.URL.Query().Get("jsFunc")
-	w.Header().Set("Content-Type", "text/plain")
-	vm := newJSRuntime()
-	v, err := vm.Run(script)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	val, _ := v.ToString()
-	w.Write([]byte(val))
-}
-
-func apiProxyRequests(w http.ResponseWriter, r *http.Request) {
-	c := session.DB("checkSite").C("requests")
+func apiProxyRequests(g *gin.Context) {
+	c := db.C("requests")
 	var pages []crawlbase.Page
 	c.Find(nil).All(&pages)
-	k, _ := json.Marshal(pages)
-	w.Write(k)
+	g.JSON(200, pages)
 }
 
-func apiDynamicCrawlRequest(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("url")
-	page := crawlDynamic(url)
-
-	validater := htmlcheck.Validator{}
-	tags := loadTagsFromFile()
-	validater.AddValidTags(tags)
-	// first check
-
-	htmlErrors := validater.ValidateHtmlString(page.Body)
-	resp := Response{page, htmlErrors}
-
-	b, err := json.Marshal(resp)
-	logFatal(err)
-	w.Write(b)
-}
-
-func apiCrawlRequest(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("url")
-	page := crawl(url)
-	validater := htmlcheck.Validator{}
-	tags := loadTagsFromFile()
-	validater.AddValidTags(tags)
-	// first check
-
-	htmlErrors := validater.ValidateHtmlString(page.Body)
-	resp := Response{page, htmlErrors}
-
-	b, err := json.Marshal(resp)
-	logFatal(err)
-	w.Write(b)
-}
-
-func apiAddTag(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusNotFound)
+func apiGetSiteInfo(g *gin.Context) {
+	url, ok := g.GetQuery("url")
+	if !ok {
+		g.String(500, "missing parameter url")
 		return
 	}
-	r.ParseForm()
-	tName := r.Form.Get("TagName")
-	aName := r.Form.Get("AttributeName")
+	_, inclBody := g.GetQuery("body")
+
+	cw := crawlbase.NewCrawler()
+	cw.Header.Add("User-Agent", userAgentHeader)
+	tags, _ := crawlbase.LoadTagsFromFile("tags.json")
+	cw.Validator.AddValidTags(tags)
+
+	page, err := cw.GetPage(url, "GET")
+
+	if err != nil {
+		g.String(500, err.Error())
+	}
+
+	// first check
+
+	if !inclBody {
+		page.RespInfo.Body = ""
+	}
+
+	g.JSON(200, page)
+}
+
+func apiAddTag(g *gin.Context) {
+	if g.Request.Method != "POST" {
+		g.String(404, "not a get")
+		return
+	}
+
+	tName, _ := g.GetQuery("TagName")
+	aName, _ := g.GetQuery("AttributeName")
 
 	if tName == "" {
-		w.Write([]byte("TagName is empty"))
+		g.String(200, "TagName is empty")
 		return
 	}
 
-	tags := loadTagsFromFile()
+	tags, _ := crawlbase.LoadTagsFromFile("tags.json")
 	tag, ok := findTag(tags, tName)
 	if !ok {
-		tag = &htmlcheck.ValidTag{tName, []string{}, false}
-		tags = append(tags, *tag)
+		tag = &htmlcheck.ValidTag{tName, []string{}, "", false}
+		tags = append(tags, tag)
 	}
+
 	ok = findString(tag.Attrs, aName)
 	if !ok {
 		tag.Attrs = append(tag.Attrs, aName)
 	}
 
-	t, _ := findTag(tags, tName)
+	//t, _ := findTag(tags, tName)
+	crawlbase.WriteTagsToFile(tags, "tags.json")
 
-	log.Println(tName, aName, tag.Attrs)
-	log.Println(t)
-	writeTagsToFile(tags)
-
-	w.Write([]byte("OK"))
+	g.String(200, "ok")
 }
 
 func findString(arrs []string, name string) bool {
@@ -234,142 +153,13 @@ func findString(arrs []string, name string) bool {
 	return false
 }
 
-func findTag(tags []htmlcheck.ValidTag, tagName string) (*htmlcheck.ValidTag, bool) {
+func findTag(tags []*htmlcheck.ValidTag, tagName string) (*htmlcheck.ValidTag, bool) {
 	for i, v := range tags {
 		if v.Name == tagName {
-			return &tags[i], true
+			return tags[i], true
 		}
 	}
 	return nil, false
-}
-
-func writeTagsToFile(tags []htmlcheck.ValidTag) {
-	b, err := json.Marshal(tags)
-	logFatal(err)
-	ioutil.WriteFile("tags.json", b, 755)
-}
-
-func loadTagsFromFile() []htmlcheck.ValidTag {
-	content, err := ioutil.ReadFile("tags.json")
-	logFatal(err)
-
-	var validTags []htmlcheck.ValidTag
-	err = json.Unmarshal(content, &validTags)
-	logFatal(err)
-
-	return validTags
-}
-
-type PhJsPage struct {
-	Body       string
-	JSwrites   []string
-	JSevals    []string
-	JStimeouts []string
-	Cookies    []crawlbase.Cookie
-	Requests   []string
-}
-
-func PageFromResponse(req *http.Request, res *http.Response, timeDur time.Duration) *crawlbase.Page {
-	page := crawlbase.Page{}
-	body, err := ioutil.ReadAll(res.Body)
-
-	if err == nil {
-		page.Body = string(body)
-		ioreader := bytes.NewReader(body)
-		doc, err := goquery.NewDocumentFromReader(ioreader)
-		if err == nil {
-			page.Hrefs = crawlbase.GetHrefs(doc, req.URL)
-			page.Forms = crawlbase.GetFormUrls(doc, req.URL)
-			page.Ressources = crawlbase.GetRessources(doc, req.URL)
-		}
-	}
-
-	page.CrawlTime = int(time.Now().Unix())
-	page.URL = req.URL.String()
-	page.RequestURI = req.RequestURI
-	page.Uid = crawlbase.ToSha256(page.URL)
-	page.RespCode = res.StatusCode
-	page.RespDuration = int(timeDur.Seconds() * 1000)
-	return &page
-}
-
-func crawlDynamic(urlStr string) *crawlbase.Page {
-	timeStart := time.Now()
-	res, err := http.Get("http://localhost:8079/?url=" + url.QueryEscape(urlStr))
-	logFatal(err)
-	timeDur := time.Now().Sub(timeStart)
-
-	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	logFatal(err)
-
-	PhJsPage := PhJsPage{}
-
-	err = json.Unmarshal(body, &PhJsPage)
-	logFatal(err)
-
-	page := crawlbase.Page{}
-	page.Body = PhJsPage.Body
-
-	ioreader := bytes.NewReader([]byte(page.Body))
-	doc, err := goquery.NewDocumentFromReader(ioreader)
-	logFatal(err)
-
-	baseUrl, err := url.Parse(urlStr)
-	logFatal(err)
-
-	page.Hrefs = crawlbase.GetHrefs(doc, baseUrl)
-	page.Forms = crawlbase.GetFormUrls(doc, baseUrl)
-	page.Ressources = crawlbase.GetRessources(doc, baseUrl)
-
-	page.CrawlTime = int(time.Now().Unix())
-	page.URL = urlStr
-	page.RespCode = 200
-	page.RespDuration = int(timeDur.Seconds() * 1000)
-	page.Uid = crawlbase.ToSha256(urlStr)
-	page.Cookies = PhJsPage.Cookies
-
-	jsinfos := []crawlbase.JSInfo{}
-	for _, v := range PhJsPage.JSwrites {
-		info := crawlbase.JSInfo{"document.write", v}
-		jsinfos = append(jsinfos, info)
-	}
-	for _, v := range PhJsPage.JSevals {
-		info := crawlbase.JSInfo{"eval", v}
-		jsinfos = append(jsinfos, info)
-	}
-	for _, v := range PhJsPage.JStimeouts {
-		info := crawlbase.JSInfo{"setTimeout", v}
-		jsinfos = append(jsinfos, info)
-	}
-	for _, v := range PhJsPage.Requests {
-		info := crawlbase.Ressource{v, "", "", ""}
-		page.Requests = append(page.Requests, info)
-	}
-
-	page.JSInfo = jsinfos
-
-	return &page
-}
-
-func crawl(urlStr string) *crawlbase.Page {
-	timeStart := time.Now()
-	client := http.Client{Timeout: 30 * time.Second}
-	req, err := http.NewRequest("GET", urlStr, nil)
-	logFatal(err)
-
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36")
-
-	res, err := client.Do(req)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	timeDur := time.Now().Sub(timeStart)
-
-	page := PageFromResponse(req, res, timeDur)
-	return page
 }
 
 func logFatal(err error) {
